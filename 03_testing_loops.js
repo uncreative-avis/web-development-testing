f27737b992603441323c6d1215d33f659d448ed7
Avis <alihassan020808@gmail.com>
2026-08-18 15:59:33 +0100

    Add while loop test

--- 03_testing_loops.js
+++ 03_testing_loops.js
@@ -4,3 +4,11 @@
 for (let count = 1; count <= 10; count++) { // counts from 1-10, inclusive (<=)
     console.log(`Count: ${count}`)
 }
+
+// while loops as well; just need a let variable
+
+let example = 0
+while (example < 20) {
+    example += 2
+    console.log(`Loop Count: ${example}`)
+}
